0c72f2d6c5bd509778878bff93e0315eee492f13
NAZIYASHAIK1695 <101784632+NAZIYASHAIK1695@users.noreply.github.com>
2022-08-29 11:03:49 +0530

    Update data.sh

--- data.sh
+++ data.sh
@@ -4,7 +4,7 @@ sudo yum install -y yum-utils
 sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
 sudo yum -y install terraform
 sudo yum install git -y
-git clone https://github.com/amar-m-cloud/Deploy-Abalone-Age-Prediction-TF.git
+git clone https://github.com/NAZIYASHAIK1695/Deploy-Abalone-Age-Prediction-TF.git
 cd Deploy-Abalone-Age-Prediction-TF
 sudo terraform init
 sudo terraform apply -auto-approve
